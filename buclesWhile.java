e31e43b67cf9027b0801571e18aadc6756a9daaa
Hagakure <pablojesus93@hotmail.com>
2024-07-10 13:07:42 +0200

    Arreglo close

--- buclesWhile.java
+++ buclesWhile.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 public class buclesWhile {
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        int num = 0;
-        do {
-            System.out.println("Introduce un número distinto de cero para seguir en el bucle");
-            num = teclado.nextInt();
-        } while (num != 0);
-        System.out.println("Fin del bucle");
-        teclado.close();
+        try (Scanner teclado = new Scanner(System.in)) {
+            int num;
+            do {
+                System.out.println("Introduce un número distinto de cero para seguir en el bucle");
+                num = teclado.nextInt();
+            } while (num != 0);
+            System.out.println("Fin del bucle");
+            teclado.close();
+        }
 
     }
 
