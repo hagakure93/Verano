7c06457c941c73da8e709ba9d4c2a0cc72b9dc85
Hagakure <pablojesus93@hotmail.com>
2024-07-13 10:21:58 +0200

    Media

--- bucleFor.java
+++ bucleFor.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class bucleFor {
     public static void main(String[] args) {
         double media = 0; // las variables en minúscula 
-        Scanner entrada = new Scanner(System.in);
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Dime un número ");
-            int num = entrada.nextInt();  // El import aparece cuando pongo el nextInt
-            System.out.println("El primer número introducido es: "+i);
-            media = media += num;
-            
+        try (Scanner entrada = new Scanner(System.in)) {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("Dime un número ");
+                int num = entrada.nextInt();  // El import aparece cuando pongo el nextInt
+                System.out.println("El primer número introducido es: "+i);
+                media = media += num;
+                
+            }
+            System.out.println("La media de los cinco números introducidos es: "+media/5);
         }
-        System.out.println("La media de los cinco números introducidos es: "+media/5);
-        entrada.close();
     }
     
 }
