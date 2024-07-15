efe02854306a8355df32529c5e7000e54bafb639
Hagakure <pablojesus93@hotmail.com>
2024-07-15 11:34:27 +0200

    Ejercicios

---
+++ primerEjerc.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class primerEjerc {
+
+    public static void main(String[] args) {
+        try (Scanner entrada = new Scanner(System.in)) {
+            int numLeer, numActual;
+            int max = 0, min = 0, contadorMax = 1, contadorMin = 1;
+            System.out.println("Dime una cantidad de números que leer" + (numLeer = entrada.nextInt()));
+            for (int i = 0; i < numLeer; i++) {
+
+                System.out.println("Dime un número");
+                numActual = entrada.nextInt();
+                max = numActual;
+                min = numActual;
+
+                if (numActual > max) {  // los == :(
+                    max = numActual;
+                    contadorMax = 1;  // como es nuevo máximo , el contador se reinicia a 1
+                } else if (numActual == max) {
+                    contadorMax++;
+                }
+
+                if (numActual < min) {
+                    min = numActual;
+                    contadorMin = 1;
+                } else if (numActual == min) {
+                    contadorMin++;
+                }
+
+            }
+            System.out.println("El mayor número introducido ha sido el " + max + "y aparece " + contadorMax + " veces.");
+            System.out.println("El menor número ha sido " + min + " y aparece " + contadorMin + " veces.");
+        }
+    }
+
+}
