e6dae45669f897d4885d2c0a60b831a1ad529999
Hagakure <pablojesus93@hotmail.com>
2024-07-18 18:39:35 +0200

    Palíndromo

---
+++ Exercises/EjerSiete.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EjerSiete {
+
+    public static void main(String[] args) {
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Dime una frase");
+            String frase = entrada.nextLine().trim();
+            int palabras = 1;
+            
+            for (int i = 0; i < frase.length() - 1; i++) {
+                if (frase.charAt(i) == ' ' && frase.charAt(i + 1) != ' ')
+                    palabras++;
+            }
+            
+            if (frase.equals(""))
+                System.out.println("La frase no tiene ninguna palabra");
+            else
+                System.out.println("La frase tiene " + palabras + " palabra" + (palabras > 1 ? "s" : ""));
+        }
+    }
+
+}
