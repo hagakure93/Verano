182c89b21f44bfa4107167d38a54562e51ddca58
Hagakure <pablojesus93@hotmail.com>
2024-07-18 17:05:04 +0200

    Descomp palabra

---
+++ Exfive.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Exfive {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Dime un número y lo descompongo");
+        String palabra = entrada.nextLine();
+
+        for (int i = 0; i < palabra.length(); i++) {
+            System.out.print(palabra.charAt(i) + " "); //primera vez que no uso el println
+            
+            }
+            System.out.println();
+    entrada.close();
+}
+    
+}
--- fourthEx.java
+++ fourthEx.java
@@ -13,31 +13,31 @@ public class fourthEx {
 
     public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
-        String resultado = "";
-        int total = 0;
-        int num = 0;
-
-        do {
-            System.out.println("Introduce un número positivo , cero para terminar.");
-            num = entrada.nextInt();
-            int cifras = 1;
-            int suma = num % 10;  // esto se hace para guardar el último número en variable suma
-            int aux = num; // crea variable aux para no trabajar directo con num y perderlo
-
-            while (aux > 9) {
-                aux /= 10;
-                cifras++;
-                suma += aux % 10;
-            }
-            if (cifras == suma) {   // esta es la condición del ejercicio
-                resultado += num + "+";
-                total += num;
-            }
-
-        } while (num != 0);
-        System.out.println(resultado + "\b=" + total);  // la B elimina el ultimo de la cadena que es un +
-        entrada.close();
+        try (Scanner entrada = new Scanner(System.in)) {
+            String resultado = "";
+            int total = 0;
+            int num;
+            
+            do {
+                System.out.println("Introduce un número positivo , cero para terminar.");
+                num = entrada.nextInt();
+                int cifras = 1;
+                int suma = num % 10;  // esto se hace para guardar el último número en variable suma
+                int aux = num; // crea variable aux para no trabajar directo con num y perderlo
+                
+                while (aux > 9) {
+                    aux /= 10;
+                    cifras++;
+                    suma += aux % 10;
+                }
+                if (cifras == suma) {   // esta es la condición del ejercicio
+                    resultado += num + "+";
+                    total += num;
+                }
+                
+            } while (num != 0);
+            System.out.println(resultado + "\b=" + total);  // la B elimina el ultimo de la cadena que es un +
+        }
 
     }
 
