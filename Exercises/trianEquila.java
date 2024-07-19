35cf6d7b42059fb99d99671999e10c9f2c1b4368
Hagakure <pablojesus93@hotmail.com>
2024-07-19 18:03:36 +0200

    trianguloEquila

---
+++ Exercises/trianEquila.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class trianEquila {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce la altura del triángulo");
+        int altura = entrada.nextInt();
+        int base = altura*2-1;
+        System.out.println();
+
+        for (int i = 0; i < altura; i++) {
+
+            for (int j = 0; j < base; j++) {
+                // Con esto busca el índice central , añadiendo 2 asterisc , uno a cada lado
+                if (j>= base / 2 - i && j <= base / 2 + i){  // el entre 2 es para hallar el centro del trngl
+                    System.out.print("* ");
+
+                } 
+                else 
+                    System.out.print("  ");
+                
+        
+            }
+            System.out.println();
+        }
+
+    }
+
+}
