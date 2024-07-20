b704c45da3efd48c8305258edac9203ee9e619d5
Hagakure <pablojesus93@hotmail.com>
2024-07-20 11:23:11 +0200

    Carpetas

---
+++ Tema 3 - Métodos/Metodos.java
@@ -0,0 +1,17 @@
+
+public class Metodos {
+
+    public static void main(String[] args) {
+
+        int num1 = 2, num2 = 4;
+        int num3 = suma(num1, num2);
+        System.out.println(num3);
+
+    }
+
+    static int suma(int a, int b) {
+
+        return a + b;
+    }
+
+}
