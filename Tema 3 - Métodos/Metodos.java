818c2452354d7330e068600997e0100a433769a8
Hagakure <pablojesus93@hotmail.com>
2024-07-20 11:57:13 +0200

    Métodos

--- Tema 3 - Métodos/Metodos.java
+++ Tema 3 - Métodos/Metodos.java
@@ -4,14 +4,30 @@ public class Metodos {
     public static void main(String[] args) {
 
         int num1 = 2, num2 = 4;
-        int num3 = suma(num1, num2);
+        int num3 = suma(num1, num2); // invoca el método suma
         System.out.println(num3);
+        System.out.println(areaCuadrado(3)); // invoca directo al metodo areaCuadrado
+        prueba(); // esto también es invocar
+        
 
     }
 
-    static int suma(int a, int b) {
+    static void prueba() {
+        System.out.println("Prueba buena");
+        
+        
+        // no hace falta poner return , pero puedes
+    }
 
-        return a + b;
+    static int suma(int a, int b) { // esto es un método
+
+        return a + b;  // o poner una variable resultado = a+b y luego poner return resultado
+    }
+
+    static int areaCuadrado(int lado) {
+
+        return lado * lado;
+
     }
 
 }
