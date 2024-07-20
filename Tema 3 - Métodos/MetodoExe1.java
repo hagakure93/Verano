b96fb8826b5c2012b45cd553644bc53d33863c79
Hagakure <pablojesus93@hotmail.com>
2024-07-20 17:36:23 +0200

    EjerciciosMetodos

--- Tema 3 - Métodos/MetodoExe1.java
+++ Tema 3 - Métodos/MetodoExe1.java
@@ -1,21 +1,22 @@
 
 import java.util.Scanner;
 
-
 public class MetodoExe1 {
+
     public static void main(String[] args) {
-        
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Dime tu nombre");
-        String nombre = entrada.nextLine();
-        salu2(nombre);   // las variables aquí , no tienen nada que ver con las del método , son perímetros
-        
-    }
 
-    static void salu2 (String nombre){   //Aqui le he dado perímetros
-       
-        System.out.println("Yo te saludo "+nombre);
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Dime tu nombre");
+            String nombre = entrada.nextLine();
+            salu2(nombre);   // las variables aquí , no tienen nada que ver con las del método , son perímetros
+        }
 
     }
-    
+
+    static void salu2(String nombre) {   //Aqui le he dado perímetros
+
+        System.out.println("Yo te saludo " + nombre);
+
+    }
+
 }
---
+++ Tema 3 - Métodos/MetodoExe2.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+public class MetodoExe2 {
+
+    public static void main(String[] args) {
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Dime un número");
+            int numero = entrada.nextInt();
+            System.out.println("El cubo de tu número es: " + cubo(numero));
+            System.out.println("Si multiplicamos 8x9 nos da: " + mult(8, 9)); // no olvides el +
+            System.out.println("Dime de qué número quieres saber la tabla de multiplicar y si es par o impar");
+            int numerito = entrada.nextInt();
+            System.out.println();
+            tablaMult(numerito);// al invocar al método , ya imprime , no tienes que hacer otro systout si el método lo tiene
+            System.out.println();
+            mostrarParImpar(numerito);
+        }
+
+    }
+
+    static int cubo(int numero) {
+
+        return (int) Math.pow(numero, 3);
+
+    }
+
+    static int mult(int a, int b) {
+        return a * b;
+    }
+
+    static void tablaMult(int num) {
+
+        System.out.println("La tabla de multiplicar del número " + num + " es:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " x " + num + " = " + mult(num, i)); // te olvidas los + siempre
+
+        }
+    }
+
+    static boolean esPar(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    static void mostrarParImpar(int n) {
+        if (esPar(n)) {
+            System.out.println("Tu número es par");
+        } else {
+            System.out.println("Tu número no es par");
+        }
+    }
+
+}
