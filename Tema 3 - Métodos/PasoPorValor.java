f839636653eb1882e2e68f95bef40397cb7f3766
Hagakure <pablojesus93@hotmail.com>
2024-07-22 16:47:52 +0200

    Paso2

--- Tema 3 - Métodos/PasoPorValor.java
+++ Tema 3 - Métodos/PasoPorValor.java
@@ -1,10 +1,12 @@
 public class PasoPorValor {
+
+    static int num2 = 2; // esto es una variable global , si no hubiese una local , se usaría esta
     public static void main(String[] args) {
 
         int num = 10;   // la variable original no cambia , se pasa una copia del valor
         int doble = doble(num); // en variable doble se guarda un 20 , pero num sigue siendo 10
         System.out.println("num= "+num+"  doble = "+doble);
-        
+        System.out.println(num2+" Esta es la variable global");
         
 
     }
