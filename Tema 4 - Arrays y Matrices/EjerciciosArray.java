60e7e76803231325e3b0f701a2b0a78767f9d523
hagakure <pablojesus93@hotmail.com>
2024-08-02 11:25:01 +0200

    Ejercicios con Arrays

--- Tema 4 - Arrays y Matrices/EjerciciosArray.java
+++ Tema 4 - Arrays y Matrices/EjerciciosArray.java
@@ -3,10 +3,45 @@ public class EjerciciosArray {
         int[] array = { 1, 34, 53, 66, 89, 66, 78, 23, 66, 88, 78, 0, 12, 21, 12 }; // aquí he dado los valores al array
         cantidadVeces(array, 78); // invoco el metodo y paso un array antes definido y un entero
 
-        String[] palabras = { "a", "aa", "aaa", "bbb", "esta es la cadena más larga" };
-        System.out.println("La string más larga es: " + cadenaMasLarga(palabras));
+        String[] palabras = { "a", "aa", "Aaa", "bbb", "esta es la cadena más larga" };
+        System.out.println("La string más larga es: \"" + cadenaMasLarga(palabras) + "\"");
+        int[] array1 = {1,2,3,4,1,13,8};
+        int[] array2 = {8,1,5,2,7,9,1};
+
+        
+        ejercicio3(palabras, 'a');
+        mostrarArray(comparaArray(array1, array2));
 
     }
+
+    static int[] comparaArray(int[] array1, int[]array2){
+
+        int[] c;
+        if (array1.length > array2.length)
+            c = new int[array2.length];
+        else
+            c = new int[array1.length];
+
+        for (int i = 0; i < c.length; i++) {  //compara lo que mida el array más pequeño 
+            if (array1[i] > array2[i])
+                c[i] = array1[i];
+            else
+                c[i] = array2[i];
+        }
+        return c;
+
+
+    }
+
+    static void mostrarArray(int[] m){
+        for (int i = 0; i < m.length; i++) {
+            System.out.print("["+m[i]+"]");
+            
+        }
+        System.out.println();
+    }
+
+
 
     static void cantidadVeces(int[] array, int numero) { // metodo pidiendo un array de enteros y un entero
 
@@ -23,15 +58,26 @@ public class EjerciciosArray {
         // hice una ternaria porque soy super listo
     }
 
-    static String cadenaMasLarga(String[] cadena) {
-        String palabraMax = "";
+    static String cadenaMasLarga(String[] cadena) { // metodo devuelve una String , pidiendo una array de Strings.
+        String palabraMax = ""; // aquí vamos guardando la palabra que sea más larga en cada vuelta
 
-        for (String cadena1 : cadena) {
-            if (cadena1.length() > palabraMax.length()) {
-                palabraMax = cadena1;
+        for (String cadena1 : cadena) { // bucle for each , como una ternaria con el if/else , pero para bucles for
+            if (cadena1.length() > palabraMax.length()) { // compara cada palabra del array con la anterior en cada
+                                                          // pasada
+                palabraMax = cadena1; // si la nueva palabra es más larga , la asigna a palabraMax
             }
         }
         return palabraMax;
+
+    }
+
+    static void ejercicio3(String[] cadenaPalabras, char letra) {
+
+        for (int i = 0; i < cadenaPalabras.length; i++) {
+            if (cadenaPalabras[i].toLowerCase().charAt(0) == Character.toLowerCase(letra))
+                System.out.println(cadenaPalabras[i]);
+
+        }
     }
 
 }
