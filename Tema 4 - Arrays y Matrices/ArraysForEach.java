5564260042a9917c294466ef07318dae9608d9cb
hagakure <pablojesus93@hotmail.com>
2024-08-06 15:03:16 +0200

    Ejercicios matrices

--- Tema 4 - Arrays y Matrices/ArraysForEach.java
+++ Tema 4 - Arrays y Matrices/ArraysForEach.java
@@ -5,22 +5,22 @@ public class ArraysForEach {
 
         int[][] array4 = { { 3, 8, 5 }, { 4, 1, 8, 4 }, { 5, 2 } };
         for (int[] fila : array4) { // Bucle for each , como el bucle for pero sin declarar cosas
-            System.out.println(Arrays.toString(fila));
+            System.out.println(Arrays.toString(fila)); // para imprimir matrices tambien , pero mejor el deep
         }
         System.out.println();
 
         int[] array = { 1, 4, 5, 7, 8, 9 };
-        for (int n : array) {
+        for (int n : array) {   // Bucle for each de un array unidimensional
             System.out.print(n + " ");
 
         }
         System.out.println();
 
-        System.out.println(Arrays.toString(array)); // para imprimir un array unidimensional
+        System.out.println(Arrays.toString(array)); // para imprimir un array unidimensional más fácilmente
 
         int[][] arrayBidimensional = { { 12, 3, 5 }, { 5, 7, 87, 6 }, { 67, 74, 23, 10 } };
         System.out.println();
-        System.out.println(Arrays.deepToString(arrayBidimensional)); // esto imprime matrices 
+        System.out.println(Arrays.deepToString(arrayBidimensional)); // esto imprime matrices
 
     }
 
---
+++ Tema 4 - Arrays y Matrices/EjercicioMatriz.java
@@ -0,0 +1,58 @@
+public class EjercicioMatriz {
+
+    public static void main(String[] args) {
+
+        imprimirMatriz(crearMatriz(4));
+        imprimirMatrizInvertida(crearMatriz(4));
+
+    }
+
+    static void imprimirMatriz(int[][] m) {
+
+        for (int[] fila : m) {
+            for (int columna : fila) {
+                if (columna == 1)
+                    System.out.print("X ");
+                else
+                    System.out.print("- ");
+
+            }
+            System.out.println();
+
+        }
+    }
+
+    static int[][] crearMatriz(int n) {
+        int[][] array1 = new int[n][n]; // esto crea la matriz , con filas y columnas
+        for (int i = 0; i < array1.length; i++) { // cantidad de filas
+
+            for (int j = 0; j < array1[i].length; j++) { // columnas
+
+                if (i == j)
+                    array1[i][j] = 1;
+                else
+                    array1[i][j] = 0;
+
+            }
+
+        }
+        return array1;
+
+    }
+
+    static void imprimirMatrizInvertida(int[][] m) {
+
+        for (int[] fila : m) {
+            String linea = "";
+            for (int columna : fila) {
+                if (columna == 1)
+                    linea = "X " + linea;
+                else
+                    linea = "- "+linea;
+            }
+            System.out.println(linea);
+
+        }
+    }
+
+}
