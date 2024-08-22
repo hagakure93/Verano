979c8b762a5146efbb861cee2c295c6228ead1d7
hagakure <pablojesus93@hotmail.com>
2024-08-22 16:23:34 +0200

    Ejercicio maximos y mínimos de una matriz

--- Tema 4 - Arrays y Matrices/GeneradorArrays.java
+++ Tema 4 - Arrays y Matrices/GeneradorArrays.java
@@ -5,19 +5,68 @@ import java.util.Random;
 public class GeneradorArrays {
     public static void main(String[] args) {
         int[][] nums = generarMatriz(4, 5, 4, 5, 9);
+        System.out.println("\nMatriz Original:");
         for (int[] fila : nums) {
             System.out.println(Arrays.toString(fila));
 
         }
 
+        int[] maximosColumnas = maximosColumnas(nums);
+        System.out.println("\nMáximos columnas: \n"+Arrays.toString(maximosColumnas));
+
+        int[] maximosFilas = maximosFilas(nums);
+        System.out.println("\nMáximos filas: \n"+Arrays.toString(maximosFilas));
+
     }
+
+    static int[] maximosColumnas(int[][] matriz) {
+
+        int anchoMax = 0;
+        for (int[] fila : matriz) {
+            if (fila.length > anchoMax)
+                anchoMax = fila.length;
+        }
+        int[] arrayMaximos = new int[anchoMax];
+        for (int i = 0; i < arrayMaximos.length; i++) {
+            arrayMaximos[i] = Integer.MIN_VALUE;
+        }
+        for (int[] matriz1 : matriz) {
+            for (int j = 0; j < matriz1.length; j++) {
+                if (matriz1[j] > arrayMaximos[j]) {
+                    arrayMaximos[j] = matriz1[j];
+                }
+            }
+        }
+        return arrayMaximos;
+    }
+
+    static int[] maximosFilas(int[][] matriz) {
+
+        int maxFila;
+        int[] arrayMaximos = new int[matriz.length];
+       
+        for (int i = 0; i < matriz.length; i++) {
+            maxFila = Integer.MIN_VALUE;
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] > maxFila)
+                    maxFila = matriz[i][j];
+            }
+            arrayMaximos[i] = maxFila;
+        }
+        return arrayMaximos;
+    }
+
     /**
-     *  Genera una matriz irregular con enteros aleatorios
+     * Genera una matriz irregular con enteros aleatorios
+     * 
      * @param anchoMin - Anchura mínima de las filas de la matriz
      * @param anchoMax - Anchura máxima de las filas de la matriz
-     * @param altoMin - Altura mínima de la matriz , debe ser uno o superior , de lo contrario podría no tener filas.
-     * @param altoMax - Cantidad máxima de filas de la matriz , debe ser superior a altoMin , si no , se igualaran.
-     * @param numMax - Valor máximo para cada uno de los enteros de la matriz , desde 0 hasta numMax.
+     * @param altoMin  - Altura mínima de la matriz , debe ser uno o superior , de
+     *                 lo contrario podría no tener filas.
+     * @param altoMax  - Cantidad máxima de filas de la matriz , debe ser superior a
+     *                 altoMin , si no , se igualaran.
+     * @param numMax   - Valor máximo para cada uno de los enteros de la matriz ,
+     *                 desde 0 hasta numMax.
      * @return - Matriz con enteros generados de forma aleatoria.
      */
 
