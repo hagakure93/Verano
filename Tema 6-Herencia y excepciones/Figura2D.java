5a74db21366ca5f217469755c99be5befbd9cb29
hagakure <pablojesus93@hotmail.com>
2024-10-15 16:55:34 +0200

    App triguantulos clases abstractas

---
+++ Tema 6-Herencia y excepciones/Figura2D.java
@@ -0,0 +1,10 @@
+public abstract class Figura2D {
+    private final int numeroLados;
+
+    public Figura2D(int numeroLados) {
+        this.numeroLados = numeroLados;
+    }
+
+    public abstract double area();
+
+}
