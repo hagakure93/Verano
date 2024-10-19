25b4d675a093df285cc567688c3d293108045af8
hagakure <pablojesus93@hotmail.com>
2024-10-19 09:53:39 +0200

    App figuras funciona

--- Tema 6-Herencia y excepciones/AppFiguras.java
+++ Tema 6-Herencia y excepciones/AppFiguras.java
@@ -8,23 +8,23 @@ public class AppFiguras {
 
         // no triángulo
         Triangulo t1 = new Triangulo(p1, p2, p3);
-        t1.toString();
+        System.out.println(t1.toString());
 
         // isósceles
         p2 = new Punto2D(1, 0);
         p3 = new Punto2D(0, 1);
         t1 = new Triangulo(p1, p2, p3);
-        t1.toString();
+        System.out.println(t1.toString());
 
         // escaleno
         p3 = new Punto2D(0, 2);
         t1 = new Triangulo(p1, p2, p3);
-        t1.toString();
+        System.out.println(t1.toString());
 
         // equilátero
         p3 = new Punto2D(0.5, 0.5 * Math.sqrt(3));
         t1 = new Triangulo(p1, p2, p3);
-        t1.toString();
+        System.out.println(t1.toString()); 
     }
 
 }
