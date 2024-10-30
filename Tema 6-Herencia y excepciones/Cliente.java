60f63cd776f3083dc2583ec1ae888c9915661e51
hagakure <pablojesus93@hotmail.com>
2024-10-30 10:35:48 +0100

    Ejercicio final Herencia

--- Tema 6-Herencia y excepciones/Cliente.java
+++ Tema 6-Herencia y excepciones/Cliente.java
@@ -1,15 +1,33 @@
+import java.time.LocalDate;
+
 public class Cliente {
-    String nombre;
 
-    @Override
-    public String toString() {
-        return "Cliente [nombre=" + nombre + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
-    }
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String localidad;
+    private LocalDate fechaDeNacimiento;
 
-    public Cliente(String nombre){
+    //////////////////////////////////////////////////////
+    // CONSTRUCTORES
+    //////////////////////////////////////////////////////
+
+    public Cliente(String nombre) {
         this.nombre = nombre;
-        
     }
+
+    public Cliente(String nombre, String apellidos, String direccion,
+            String localidad, LocalDate fechaDeNacimiento) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.localidad = localidad;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    //////////////////////////////////////////////////////
+    // GETTERS & SETTERS
+    //////////////////////////////////////////////////////
 
     public String getNombre() {
         return nombre;
@@ -18,5 +36,53 @@ public class Cliente {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public LocalDate getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    //////////////////////////////////////////////////////
+    // MÉTODOS
+    //////////////////////////////////////////////////////
+
+    public String nombreCompleto() {
+        return nombre + " " + apellidos;
+    }
+
+    public String direccionCompleta() {
+        return direccion + ", " + localidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [nombre=" + nombre + "]";
+    }
 }
