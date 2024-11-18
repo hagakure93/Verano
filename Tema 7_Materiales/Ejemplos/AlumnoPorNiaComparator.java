705a33064efdca3328c3b38adde31b8f419edcf9
hagakure <pablojesus93@hotmail.com>
2024-11-18 11:56:45 +0100

    Interfaz Comparable

---
+++ Tema 7_Materiales/Ejemplos/AlumnoPorNiaComparator.java
@@ -0,0 +1,10 @@
+
+
+import java.util.Comparator;
+
+public class AlumnoPorNiaComparator implements Comparator<Alumno> {
+    @Override
+    public int compare(Alumno a1, Alumno a2) {
+        return a1.getNia().compareTo(a2.getNia());
+    }
+}
---
+++ Tema 7_Materiales/Ejemplos/PersonaCentroEducativo.java
@@ -0,0 +1,5 @@
+
+
+public interface PersonaCentroEducativo {
+    public String getNombre();    
+}
