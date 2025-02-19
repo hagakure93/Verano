0301c74612ecad736ec78654ddb283e4b82bb596
hagakure <pablojesus93@hotmail.com>
2025-02-19 12:30:50 +0100

    Versión sin fallos

---
+++ tema8_Almacenamiento/SQL/EjemploMySQL_Select.java
@@ -0,0 +1,34 @@
+package tema8_Almacenamiento.SQL;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EjemploMySQL_Select {
+
+    public static void main(String[] args) {
+
+        try {
+
+            // realizamos conexión
+            Connection conex = ConexionBD.conectar("Empresa");              
+
+            // preparamos consulta y la ejecutamos
+            String query = "SELECT nombre, correo FROM contacto";
+            PreparedStatement instruccion = conex.prepareStatement(query);
+            ResultSet resultado = instruccion.executeQuery();
+
+            //analizamos el resultado de la consulta
+            while (resultado.next()) {
+                String nombre = resultado.getString("nombre");
+                String correo = resultado.getString("correo");
+                System.out.println(nombre+" ("+correo+")");
+            }           
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+}
