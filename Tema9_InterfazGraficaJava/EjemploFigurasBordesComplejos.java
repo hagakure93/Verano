0893a6c2afc11d9c46f1bce347c3357deea4fbe2
hagakure <pablojesus93@hotmail.com>
2025-03-08 18:30:17 +0100

    JPanel con Graphics2D

---
+++ Tema9_InterfazGraficaJava/EjemploFigurasBordesComplejos.java
@@ -0,0 +1,44 @@
+package Tema9_InterfazGraficaJava;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class EjemploFigurasBordesComplejos extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        float[] patron = new float[]{50,30,10,5};
+
+        // dibuja una línea en rojo con un ancho de 20, con terminaciones redondeadas        
+        g2d.setColor(Color.RED);
+        g2d.setStroke(new BasicStroke(10, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER,1,patron,1));               
+        g2d.drawLine(50, 30, 350, 30);
+
+        // dibuja el rectángulo con degradado
+        GradientPaint gradient = new GradientPaint(0, 0, Color.RED, getWidth(), getHeight(), Color.BLUE);
+        g2d.setPaint(gradient);
+        g2d.fillRect(50, 80, 300, 300);
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(5));
+        g2d.drawRect(50, 80, 300, 300);
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Figuras con bordes complejos");
+        frame.add(new EjemploFigurasBordesComplejos());
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
